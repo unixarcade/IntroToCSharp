8baa60c316b62af0a41294a21cc6c04e7b381872
unixarcade <tarturuses_shadow@yahoo.com>
2014-06-26 11:02:24 -0400

    Tryme

---
+++ ConsoleColors/ConsoleColor/ConsoleColor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(ConsoleColor);
+            string inputtype;
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var name in Enum.GetNames(type))
+            {
+                Console.BackgroundColor = (ConsoleColor)Enum.Parse(type, name);
+                Console.WriteLine(name);
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            foreach (var name in Enum.GetNames(type))
+            {
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(type, name);
+                Console.WriteLine(name);
+            }
+
+
+            inputtype = Console.ReadLine();
+        
+        
+        
+        }
+    }
+
