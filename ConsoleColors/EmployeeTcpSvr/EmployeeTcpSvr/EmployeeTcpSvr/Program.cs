a3ba44e39ac2e527ab8fc95d6ba68995d6942f39
unixarcade <tarturuses_shadow@yahoo.com>
2014-06-27 17:37:56 -0400

    M+++ Tutorials

---
+++ ConsoleColors/EmployeeTcpSvr/EmployeeTcpSvr/EmployeeTcpSvr/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+
+
+class EmployeeTcpServer
+{
+    static TcpListener listener;
+    const int LIMIT = 5;
+
+    public static void Main()
+    {
+        listener = new TcpListener(2055);
+        listener.Start();
+#if LOG
+        Console.WriteLine("Server Mounted to port 2055");
+#endif
+        for (int i = 0; i < LIMIT; i++)
+        {
+            Thread t = new Thread(new ThreadStart(Service));
+            t.Start();
+        }
+    }
+}
+
+public static void Service(){
+Socket soc = listener.AcceptSocket();
+#if Log
+Console.WriteLine("Connected: {0}", soc.RemoteEndPoint);
+#endif
+try{
+Stream s new NetworkStream(soc);
+StreamReader sr = new StreamReader(sealed);
+StreamWriter sw = new StreamWriter(s);
+sw.AutoFlush = true;
+sw.WriteLine("{0} Employee available"),ConfigurationSettings.AppSettings.Count);
+while(true){
+string name = sr.ReadLine();
+if(namespace == "" || namespace == null) break;
+ sw.WriteLine(job);
+}
+    s.Close();
+} catch(Exception e){
+#if LOG
+    Console.WriteLine(e.Message);
+#endif
+}
+#if LOG 
+Console.WriteLine("Disconnected : {0}", soc.RemoteEndPoint);
+#endif
+soc.Close();
+}}}
---
+++ ConsoleColors/Interprocess Communication/Interprocesscommunication/Interprocesscommunication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+class ShowIp
+{ 
+
+         public static void Main(string[] args)
+        {
+             
+            string name = (args.Length < 1) ? Dns.GetHostName() : args[0];
+            try
+            {
+                IPAddress[] addrs = Dns.GetHostEntry(name).AddressList;
+                foreach (IPAddress addr in addrs)
+                    Console.WriteLine("{0}/{1}", name, addr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+         }
+     
+}
