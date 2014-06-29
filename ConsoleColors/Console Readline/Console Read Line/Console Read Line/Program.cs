e89e472927a9cf7497fb4c55c7bde8aa9d87358b
unixarcade <tarturuses_shadow@yahoo.com>
2014-06-29 08:10:46 -0400

    Practice is working

---
+++ ConsoleColors/Console Readline/Console Read Line/Console Read Line/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InsertTabs
+{
+    private const int tabSize = 4;
+    private const string usageText = "Usage: INSERTTABS inputfile.txt out";
+    public static int Main(string[] args) {
+        StreamWriter writer = null;
+        if (args.Length < 2)
+        {
+            Console.WriteLine(usageText);
+            return 1;
+        } try {
+            // attempt to open output file
+            writer = new StreamWriter(args[1]);
+            // redirect standard output
+            Console.SetOut(writer);
+            // Redirect standard input
+            Console.SetIn(new StreamReader(args[0]));
+        }
+        catch (IOException)
+        {
+            TextWriter errorWriter = Console.Error;
+            errorWriter.WriteLine(e.Message);
+            errorWriter.WriteLine(usageText);
+            return 1;
+        }
+        string line;
+        while((line = Console.ReadLine()) !null) {
+            string newLine = line.Replace(("").PadRight(tabSize, ' '), "\t");
+            Console.WriteLine(newLine);
+        }
+        writer.Close();
+        //recover standad output stream
+        // completion message can be displayed
+        StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
+        standardOutput.AutoFlush = true;
+        Console.SetOut(standardOutput);
+        Console.WriteLine("INSERTTABS has comppleted the processing of {0}.", args[0]);
+    }
+}
---
+++ ConsoleColors/Indexer/indexer.cs/indexer.cs/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class FileByteArray
+{
+    Stream stream; // holds underlying stream
+
+    //Create new filebytearray
+    public FileByteArray(string FileName)
+    {
+        stream = new FileStream(FileName, FileMode.Open);
+    }
+
+    // close stream
+    public void Close()
+    {
+        stream.Close();
+        stream = null;
+    }
+
+    public byte this[long index]
+    {
+        get
+        {
+            byte[] buffer = new byte[1];
+            stream.Seek(index, SeekOrigin.Begin);
+            stream.Read(buffer, 0, 1);
+            return buffer[0];
+        }
+
+        // Write one byte at offset index and return it
+        set
+        {
+            byte[] buffer = new byte[1] { value };
+            stream.Seek(index, SeekOrigin.Begin);
+            stream.Write(buffer, 0, 1);
+        }
+    }
+
+}
+
+    class Reverse
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
