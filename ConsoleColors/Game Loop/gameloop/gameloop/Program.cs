4ddc8744eb5951744941c0e70bf9e4a66b8d97d9
unixarcade <tarturuses_shadow@yahoo.com>
2014-06-30 18:24:56 -0400

    Game Loop

---
+++ ConsoleColors/Game Loop/gameloop/gameloop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameloop
+{
+    class gameloop
+    {
+        static void Main(string[] args)
+        {
+            string myn;
+            bool mybool;
+            mybool = false;
+            do
+            {
+                Console.WriteLine("I live");
+
+
+                Console.WriteLine("If you Enter YES you will quit the program");
+                myn = Console.ReadLine();
+                myn = myn.ToUpper();
+                if (myn == "YES")
+                {
+                    mybool = true;
+                }
+
+            } while (mybool == false);
+        
+        }
+    }
+}
