9b33d958022b5724b7b8b73924395d164b2bc469
unixarcade <tarturuses_shadow@yahoo.com>
2014-06-30 12:47:57 -0400

    Casting

---
+++ ConsoleColors/Casting/Casting/Casting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class CastDemo
+    {
+        static void Main(string[] args)
+        {
+            int i = 3, j = 2;
+
+       
+            float fraction;
+            fraction = (float)i / (float)j;
+            Console.WriteLine("fraction : " + fraction);
+            Console.ReadKey();
+
+        }
+    }
+}
