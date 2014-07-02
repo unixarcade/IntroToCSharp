f8b28e82584514440b3a29e145fc1f17d71edfbe
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-02 11:04:24 -0400

    M

--- Grand Circus CSharp Tutorial/Name/Loop/Loop/Loop/Program.cs
+++ Grand Circus CSharp Tutorial/Name/Loop/Loop/Loop/Program.cs
@@ -10,6 +10,23 @@ namespace Loop
     {
         static void Main(string[] args)
         {
+            ulong number = 0;
+            while (number < 10000000000000000000)
+            {
+                Console.WriteLine(number + " " + number+" " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+                Console.WriteLine(number + " " + number + " " + number + " " + number);
+
+                number = number + 111111111111111;
+
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Grand Circus CSharp Tutorial/Name/New folder/Foreach/Foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("John Doe");
+            list.Add("Jane Doe");
+            list.Add("Someone Else");
+
+            foreach (string name in list)
+                Console.WriteLine(name);
+
+            Console.ReadLine();
+        
+        }
+    }
+}
