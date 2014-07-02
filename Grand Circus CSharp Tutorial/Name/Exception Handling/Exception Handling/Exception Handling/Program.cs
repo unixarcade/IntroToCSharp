5ee7aec0a22966f3c6ba0b68114c0208d529944a
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-02 17:29:05 -0400

    Tutorials

---
+++ Grand Circus CSharp Tutorial/Name/Exception Handling/Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[2];
+            try
+            { 
+            numbers[0] = 23;
+            numbers[1] = 32;
+            numbers[2] = 42;
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+                catch
+            {
+               Console.WriteLine("Something went wrong!");
+                }
+
+            
+    }
+}
+    }
+}}
+}
---
+++ Grand Circus CSharp Tutorial/Name/Struct/Struct/Struct/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car;
+
+            car = new Car("Blue");
+            Console.WriteLine(car.Describe());
+
+            car = new Car("Red");
+            Console.WriteLine(car.Describe());
+
+            Console.ReadKey();
+        
+        
+        }
+    }
+
+
+
+    struct Car
+    {
+        private string color;
+        public Car(string color)
+        {
+            this.color = color;
+        }
+
+
+        public string Describe()
+        {
+            return "This car is " + color;
+
+        }
+
+
+        public string Color
+        {
+            get { return color; }
+
+            set { color = value; }
+        }
+    }
+
+   
+}
