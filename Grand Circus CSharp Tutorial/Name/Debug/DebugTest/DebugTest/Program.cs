a030d6b5642388c517d8e791be3d0eea53eb8278
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-02 14:33:38 -0400

    M+++

---
+++ Grand Circus CSharp Tutorial/Name/Debug/DebugTest/DebugTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 8, c = 233;
+            int d = a + c - b;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Grand Circus CSharp Tutorial/Name/Generics/System Generics/System Generics/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System_Generics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> dogs = new List<Dog>();
+            dogs.Add(new Dog("Fido"));
+            dogs.Add(new Dog("Bob"));
+            dogs.Add(new Dog("Adam"));
+            dogs.Sort();
+
+            foreach (Dog dog in dogs)
+                Console.WriteLine(dog.Describe());
+            Console.ReadKey();
+        
+        
+        }
+    }
+
+
+    interface IAnimal
+    {
+        string Describe();
+
+        string Name
+        {
+            get;
+            set;
+        }
+
+        class Dog : IAnimal, IComparable
+        {
+            private string name;
+
+            public Dog(string name)
+            {
+                this.name = name;
+            }
+
+            public string Describe()
+            {
+                return "Hello, I am a dog and my name is " + this.name;
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj is IAnimal)
+                    return this.name.CompareTo((obj as IAnimal).Name);
+                return 0;
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+    }
+}
