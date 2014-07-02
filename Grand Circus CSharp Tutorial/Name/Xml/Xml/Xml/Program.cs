0babc13b2e6e98b75d48bc10ac6624dd3bad51e5
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-02 17:43:10 -0400

    Xml

---
+++ Grand Circus CSharp Tutorial/Name/Xml/Xml/Xml/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/euroxref/euroxref-daily.xml");
+            while(xmlReader.Read())
+            {
+                if(xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+                {
+            if (xmlReader.HasAttributes)
+                Console.WriteLine(xmlReader.GetAttribute( "currency" + ": " + xmlReader.GetAttribute("rate"));
+        }}
+        Console.ReadKey();
+
+        }
+    } 
+               }
+    
