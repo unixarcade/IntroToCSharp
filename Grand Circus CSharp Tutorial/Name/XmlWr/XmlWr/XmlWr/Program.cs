12158fdbf9a9742b9d48091f2dec3ef5137c48e7
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-03 11:05:18 -0400

    Mxml

---
+++ Grand Circus CSharp Tutorial/Name/XmlWr/XmlWr/XmlWr/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace XmlWr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("test-doc.xml");
+            XmlNodeList userNodes = xmlDoc.SelectNodes("//users/user");
+
+            foreach (XmlNode userNode in userNodes)
+            {
+                int age = int.Parse(userNode.Attributes["age"].Value);
+                userNode.Attributes["age"].Value = (age + 1).ToString();
+
+            }
+            xmlDoc.Save("test-doc.xml");
+
+
+
+        }
+    }
+}
