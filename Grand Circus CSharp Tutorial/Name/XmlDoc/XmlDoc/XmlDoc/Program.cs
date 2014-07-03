1db40eeeb28d75979017077d4101996807521bc3
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-03 09:58:13 -0400

    Xml

---
+++ Grand Circus CSharp Tutorial/Name/XmlDoc/XmlDoc/XmlDoc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlDoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://rss.cnn.com/rss/edition_world.rss");
+            XmlNode titleNode = xmlDoc.SelectSingleNode("//rss/channel/title");
+
+            if(titleNode != null){
+                Console.WriteLine(titleNode.InnerText);
+            }
+
+            Console.ReadKey();
+        
+        
+        }
+    }
+}
