5f41f36cf064a033e9e9d0956d991924b34aa22b
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-03 12:25:43 -0400

    MReflect

---
+++ Grand Circus CSharp Tutorial/Name/ReflectionTest/ReflectionTest/ReflectionTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] assemblyTypes = assembly.GetTypes();
+            foreach(Type t in assemblyTypes)
+            {
+                Console.WriteLine(t.Name);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
+
+class DummyClass
+{
+    // Wee output
+}
---
+++ Grand Circus CSharp Tutorial/Name/Type/Type/Type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "test";
+            Console.WriteLine(test.GetType().FullName);
+            Console.WriteLine(typeof(Int32).FullName);
+            Console.ReadKey();
+        }
+    }
+}
