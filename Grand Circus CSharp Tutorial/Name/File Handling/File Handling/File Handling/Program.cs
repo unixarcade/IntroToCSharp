1170f1b4b3fb46581614e82b803f4dcab20534fa
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-03 11:34:17 -0400

    File Handling

---
+++ Grand Circus CSharp Tutorial/Name/File Handling/File Handling/File Handling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("test.txt"))
+            {
+                string content = File.ReadAllText("test.txt");
+                Console.WriteLine("Current content of file: ");
+                Console.WriteLine(content);
+
+
+            }
+            Console.WriteLine("Please enter new content for the file: ");
+            string newContent = Console.ReadLine();
+            File.WriteAllText("test.txt", newContent);
+
+        }
+    }
+}
