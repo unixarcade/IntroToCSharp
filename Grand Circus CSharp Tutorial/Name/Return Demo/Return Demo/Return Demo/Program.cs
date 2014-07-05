fcc970657e7525b8cf784702551147a6d95d2ab2
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-05 13:19:00 -0400

    Return

---
+++ Grand Circus CSharp Tutorial/Name/Return Demo/Return Demo/Return Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Demo
+{
+    
+
+        class  Return_Demo{
+            static int sillyReturnPlus(int i)
+            {
+                i = i + 1;
+                Console.WriteLine("i is : " + i);
+                return i;
+            }
+
+
+        static void Main(string[] args)
+        {
+            int res;
+            res = sillyReturnPlus(5);
+            Console.WriteLine("res is : " + res);
+
+            Console.ReadKey();
+        
+        }
+    }
+}
