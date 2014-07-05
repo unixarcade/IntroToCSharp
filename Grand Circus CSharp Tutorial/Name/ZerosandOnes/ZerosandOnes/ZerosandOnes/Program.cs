9f231f809399d4ba80fd104d2e542c0b8a6b8e84
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-05 19:05:49 -0400

    ZerosandOnes

---
+++ Grand Circus CSharp Tutorial/Name/ZerosandOnes/ZerosandOnes/ZerosandOnes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZerosandOnes
+{
+    class Program
+    {
+        //luminosity
+        static void Main(string[] args)
+        {
+            int[,] board = new int[3, 3];
+            board[1,1]=1;
+            board[0, 1] = 1;
+            board[1, 0] = 1;
+            for (int x = 0; x <2; x++)
+            {
+                Console.Write(board[x, 0] + " " );
+                Console.Write(board[0, x] + " ");
+            }
+            Console.ReadKey();
+            
+        }
+   
+    
+    }
+       
+}
