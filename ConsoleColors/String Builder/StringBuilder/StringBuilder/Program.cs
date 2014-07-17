f3eccb636e85b7aefe6750f5dd60b011f8a1c06f
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-17 17:25:56 -0400

    Games Moodlight2

---
+++ ConsoleColors/String Builder/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.StringBuilder;
+
+using System.Threading.Tasks;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          Dim myStringbuilder as new System.Text.Stringbuilder;
+            
+            StringBuilder sb = new StringBuilder("To love");
+
+        }
+    }
+}
