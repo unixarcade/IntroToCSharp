6814ce411bf16d5d497dab7b0b4509ba62b54d59
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-19 10:13:26 -0400

    DNSIP
    Works, could be updated with new tech

---
+++ Networking/DnsIp/DnsIP/DnsIP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DnsIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            Console.WriteLine("Local hostname: {0}", hostName);
+            IPHostEntry myself = Dns.GetHostByName(hostName);
+            foreach (IPAddress address in myself.AddressList)
+            {
+                Console.WriteLine("Ip Adresses: {0}", address.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
