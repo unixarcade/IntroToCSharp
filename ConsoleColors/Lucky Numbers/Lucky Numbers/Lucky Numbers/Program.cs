67e0cda9acb68c72359b674c28b79c4477ac438a
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-23 20:42:49 -0400

    Maximal Platform

---
+++ ConsoleColors/Lucky Numbers/Lucky Numbers/Lucky Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 0; b <= 9; b++)
+                {
+                    for(int c = 0; c <=9; c++)
+                    {
+                        for(int d = 0; d<=9; d++)
+                        {
+                            if((a +b) == (c + d))
+                            {
+                                Console.WriteLine(
+                                    " " + a + " " + b + " " + c + " " + d);
+                            }
+                        }
+                    }
+                }
+            }
+        
+        }
+    }
+}
---
+++ ConsoleColors/TriangularArea/TriangularArea/TriangularArea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangularArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculates " + "the area of a rectangle or triangle");
+
+            Console.WriteLine(" Enter a and b for rectangle " + "or a and h for triangle:");
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Enter 1 for a rectangle or " + "2 for a triangle: ");
+
+            int choice = int.Parse(Console.ReadLine());
+            double area = (double)(a * b) / choice;
+            Console.WriteLine(" The area o your figure is " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
