7671f5066c1e1ad2426b6e383f3b4ae19be0db66
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-25 17:34:21 -0400

    Print Logo

---
+++ ConsoleColors/PrintLogoMethod/PrintLogo/PrintLogo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintLogo
+{
+    class Program
+    {
+        
+        
+        static void PrintCompanyInformation()
+        {
+            PrintLogo();
+
+            Console.WriteLine("Address: The Data");
+        }
+
+        static void PrintLogo(){
+            Console.WriteLine("Luminosity");
+            Console.WriteLine("Address: The Data");
+
+            // For a stackoverflow exception uncomment 
+            // PrintCompanyInformation();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintLogo();
+
+            Console.ReadKey();
+        }
+    }
+}
