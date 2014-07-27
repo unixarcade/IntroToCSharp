44f9c925e5e80c54cfc19b423ebb446ca2eac2ac
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-27 17:58:49 -0400

    Triag

---
+++ ConsoleColors/Triangles of Various Sizes/Triangles of Various Sizes/Triangles of Various Sizes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangles_of_Various_Sizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input number of lines you want your triangle to be below.");
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int line = 1; line <= n; line++)
+            {
+                PrintLine(1, line);
+            }
+
+            for (int line = n - 1; line >= 1; line--)
+            {
+                PrintLine(1, line);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for(int i = start; i<= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ ConsoleColors/Triangles of Various Sizes/Triangles of Various Sizes/Triangles of Various Sizes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Triangles of Various Sizes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Triangles of Various Sizes")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e7fa24d-7f11-4758-8dc1-9f20f756d63b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
