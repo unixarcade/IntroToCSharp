30c364434cb60ca3c2987e148c404bef45abb531
unixarcade <tarturuses_shadow@yahoo.com>
2014-07-27 16:46:44 -0400

    OverloadedMethod

---
+++ ConsoleColors/Overloaded Method/OverLoadedMethod/OverLoadedMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadedMethod
+{
+    class Program
+    {
+
+        static void PrintNumbers(int intValue, float floatValue)
+        {
+            Console.WriteLine(intValue + "; " + floatValue);
+        }
+
+        static void PrintNumbers(float floatValue, int intValue)
+        {
+            Console.WriteLine( floatValue + "; " + intValue);
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrintNumbers(2.71f, 2);
+            PrintNumbers(5, 3.14159f);
+
+            Console.ReadKey();
+        
+        }
+    }
+}
