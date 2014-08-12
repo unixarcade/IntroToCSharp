f1ee307481cf80eb21fd8dddbfbd77f89bc30642
unixarcade <tarturuses_shadow@yahoo.com>
2014-08-12 17:19:09 -0400

    Read File

---
+++ ConsoleColors/ReadFile/Readfile/Readfile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Readfile
+{
+    class Program
+    {
+
+
+        static void ReadFile(string fileName)
+        {
+            TextReader reader = new StreamReader(fileName);
+            try
+            {
+                for (int x = 0; x <= 9; x++)
+                {
+                    string line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+           
+            
+                ReadFile("zen.txt");
+            
+            Console.ReadKey();
+        }
+
+    }
+}
