9dc7090621833cadc709ac445f95d79f586c0835
unixarcade <tarturuses_shadow@yahoo.com>
2014-08-24 21:46:59 -0400

    Speech Synth

---
+++ ConsoleColors/Speech synth/Speech Synth/Speech Synth/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Speech_Synth;
+using System.Speech.Synthesis;
+
+
+
+
+namespace Speech_Synth
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+
+
+
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                synth.Speak("Hello World");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
