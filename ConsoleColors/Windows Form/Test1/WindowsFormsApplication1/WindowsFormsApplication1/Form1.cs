703a88867c8dd350b19fceb832f6da43e117ce50
unixarcade <tarturuses_shadow@yahoo.com>
2014-08-29 12:52:38 -0400

    A whole lotta doing for a whole lotta nothing
    What I could do in other languages in far fewer lines takes six.

---
+++ ConsoleColors/Windows Form/Test1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            Int32 a = Convert.ToInt32(textBox1.Text);
+            Int32 b = Convert.ToInt32(textBox1.Text);
+            Int32 c = a + b;
+           string d = Convert.ToString(c);
+           label1.Text = d;
+            MessageBox.Show(d); 
+            //label1.Text = a + b;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
