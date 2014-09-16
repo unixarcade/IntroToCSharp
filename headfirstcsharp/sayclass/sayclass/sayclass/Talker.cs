360b0ac1f9146c6c0633f73319bc25372e3012b5
unixarcade <tarturuses_shadow@yahoo.com>
2014-09-16 12:56:49 -0400

    Talker

---
+++ headfirstcsharp/sayclass/sayclass/sayclass/Talker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayclass
+{
+    class Talker
+    {
+        public static int BlahBlah( string thingToSay, int numberOfTimes)
+        {
+            string finalString = "";
+            for (int count = 1; count <= numberOfTimes; count++)
+            {
+                finalString = finalString + thingToSay + "\n";
+            }
+            MessageBox.Show(finalString);
+            return finalString.Length;
+        }
+    }
+}
