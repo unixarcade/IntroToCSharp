410e3440d1139eafc89e55a1e617fd83704877a2
unixarcade <tarturuses_shadow@yahoo.com>
2014-10-04 11:00:37 -0400

    to commit

---
+++ headfirstcsharp/funwithpeople/fwpeople/fwpeople/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace fwpeople
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nikkisCashLabel = new System.Windows.Forms.Label();
+            this.sharonsCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nikkisCashLabel
+            // 
+            this.nikkisCashLabel.AutoSize = true;
+            this.nikkisCashLabel.Location = new System.Drawing.Point(7, 20);
+            this.nikkisCashLabel.Name = "nikkisCashLabel";
+            this.nikkisCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.nikkisCashLabel.TabIndex = 0;
+            this.nikkisCashLabel.Text = "label1";
+            // 
+            // sharonsCashLabel
+            // 
+            this.sharonsCashLabel.AutoSize = true;
+            this.sharonsCashLabel.Location = new System.Drawing.Point(6, 48);
+            this.sharonsCashLabel.Name = "sharonsCashLabel";
+            this.sharonsCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.sharonsCashLabel.TabIndex = 1;
+            this.sharonsCashLabel.Text = "label2";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(8, 72);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give $10 Sharon";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(149, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Recieve $5 from Nikki";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Nikki gives $10 to Bob";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(149, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 39);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Sharon gives $5 to Nikki";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.sharonsCashLabel);
+            this.Controls.Add(this.nikkisCashLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nikkisCashLabel;
+        private System.Windows.Forms.Label sharonsCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ headfirstcsharp/funwithpeople/fwpeople/fwpeople/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fwpeople
+{
+    public partial class Form1 : Form
+    {
+        object Woman;
+        Woman nikki;
+        Woman sharon;
+        int bank = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            nikki = new Woman() { Cash = 100, Name = "Nikki" };
+            sharon = new Woman() { Cash = 100, Name = "Sharon" };
+            UpdateForm();
+        }
+
+        public void UpdateForm()
+        {
+            nikkisCashLabel.Text = nikki.Name + " has $" + nikki.Cash;
+            sharonsCashLabel.Text = sharon.Name + " has $" + sharon.Cash;
+            bankCashLabel.Text = " The bank has $" + bank;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= nikki.RecieveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show(" The bank is out of money.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += nikki.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nikki.RecieveCash(sharon.GiveCash(5);
+            UpdateForm();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sharon.RecieveCash(nikki.GiveCash(5));
+            UpdateForm();
+        }
+    }
+}
